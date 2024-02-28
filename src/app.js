b3441bc6525bc76c5886487635248cfcf2673b61
csumners <160021927+csumners@users.noreply.github.com>
2024-02-28 17:52:05 +0000

    Added javascript for forecast data with API integration

--- src/app.js
+++ src/app.js
@@ -53,6 +53,13 @@ function handleSearchSubmit(event) {
   searchCity(searchInput.value);
 }
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[date.getDay()];
+}
+
 function getForecast(city) {
   let apiKey = "3aa37c9co3545b897te21e51aff40c86";
   let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
@@ -60,24 +67,27 @@ function getForecast(city) {
 }
 
 function displayForecast(response) {
-  console.log(response.data);
-
-  let days = ["Tue", "Wed", "Thu", "Fri", "Sat"];
   let forecastHtml = "";
 
-  days.forEach(function (day) {
-    forecastHtml =
-      forecastHtml +
-      `
+  response.data.daily.forEach(function (day, index) {
+    if (index < 5) {
+      forecastHtml =
+        forecastHtml +
+        `
   <div class="weather-forecast-day">
-    <div class="weather-forecast-date">${day}</div>
-    <div class="weather-forecast-icon">☀️</div>
+    <div class="weather-forecast-date">${formatDay(day.time)}</div>
+    <img src="${day.condition.icon_url}" class="weather-forecast-icon"/>
     <div class="weather-forecast-temperatures">
-      <span class="weather-forecast-temperature-max"> 18° </span>
-      <span class="weather-forecast-temperature-min"> 12° </span>
+      <span class="weather-forecast-temperature-max">${Math.round(
+        day.temperature.maximum
+      )}° </span>
+      <span class="weather-forecast-temperature-min">${Math.round(
+        day.temperature.minimum
+      )}° </span>
     </div>
   </div>
 `;
+    }
   });
   let forecastElement = document.querySelector("#forecast");
   forecastElement.innerHTML = forecastHtml;
